bafc23974c8f29c7e7c1baa33b75a58c55fbdff5
ShowKa <kanamoto.official@gmail.com>
2016-12-04 21:46:03 +0900

    extending....

--- 3d.js
+++ 3d.js
@@ -6,6 +6,8 @@ const args = require("argv").run().targets;
 // lib
 const gm = require('./lib/GmExtends');
 const Coordinate = require("./lib/Coordinate");
+const Matrix = require("./lib/Matrix");
+const Vector = require("./lib/Vector");
 
 // const 
 const width = 500;
@@ -18,13 +20,14 @@ const color = args[0] ? args[0] : "white";
 let pic = new gm(width, height, color);
 
 let c = new Coordinate(0, 0, 0);
+let p1 = c.createPoint(50, 100, 0);
 
-let p1 = c.createPoint(100, 150, 200);
+let me = new Coordinate(0, 0, 1000);
+let wallPoint = me.createPoint(0, 0, -500);
+let normalVector = new Vector(0, 0, -1);
+let wall = me.createWall(normalVector, wallPoint);
 
-c.shift(1, 2, 3);
-c.rotate(Math.PI, Math.PI, Math.PI / 2);
-
-l(p1);
+l(me._getAnotherCoodinatePoint(p1));
 
 // save
 pic.write('picture/3D.png', function(err) {
